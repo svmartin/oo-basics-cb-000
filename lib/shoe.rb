2686a98e08d1c2bba160076b6c6427ee0a6efa22
Sean Martin <sean.martin@gmail.com>
2018-09-30 18:55:29 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  
+  attr_accessor :brand, :color, :size, :material, :condition
+
   def initialize(brand)
     @brand = brand
   end
